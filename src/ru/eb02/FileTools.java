d16228e9c90625a81c4cd28a60b50afa675c0ba5
Luis Zugasti <luis.zugasti@outlook.com>
2020-04-23 21:05:25 -0400

    Further modification to FileTools
    New static methods added, flushing out
    the final interface for TREC in prep
    for tests

--- src/ru/eb02/FileTools.java
+++ src/ru/eb02/FileTools.java
@@ -1,18 +1,45 @@
 package ru.eb02;
 
+/**
+ * The file tools class is a collection of simple, static helper methods that allow interfacing
+ * with the current file system. Some methods are more specialized than others.
+ *
+ * A general usage of FileTools:
+ * 1. Create your run folder with @createFilePath.
+ * 2. Read the contents of the original TREC search results file into a trecTopic object using
+ *    @openTrecSearchResultsFile.
+ * 3. Read the contents of your baseline scores with @openTrecScoresFile and save it into a
+ *    trecResult object.
+ * 4. Perform your tests; anytime you need to run a TREC test, simply use one of the helper
+ *    writer/reader methods. Also make sure to call trecEvaler which will return to you your
+ *    results!
+ *    TODO: Write the trecEvaler method.
+ *    TODO: Do a brief test of the methods here. As our interface with TREC, they are highly
+ *          important!
+ */
+
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.io.Reader;
 import java.nio.charset.Charset;
 import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.ArrayList;
+import java.util.List;
 
 public class FileTools {
-
+  public static final String GLOBALRESULTSPATH = "~/TagMe Engine Search results/";
+  /**
+   * Opens a file in ASCII encoding.
+   * @param filePath The filepath specified absolutely.
+   * @return A single string containing all of the file contents.
+   * @throws IOException In case the file specified is not found.
+   */
   public static String readFileASCII(String filePath) throws IOException {
     BufferedReader br = new BufferedReader(new FileReader(filePath));
 
@@ -35,11 +62,15 @@ public class FileTools {
     return fileText.toString();
   }
 
-  // https://stackoverflow.com/questions/4964640/reading-inputstream-as-utf-8
-  // I have read into this... so many imports! I assume it will work...
-  // No official unit tests BUT I was able to test this and it does open UTF8 nicely.
-  // I vouch for it. Wayyy better than what any unit test will give you.
-  // Theres some unit testing at the bottom. Not Junit... but good enough for me.
+  /**
+   * Opens a file in UTF8 encoding. Reference:
+   * https://stackoverflow.com/questions/4964640/reading-inputstream-as-utf-8
+   * @param filePath The filepath specified absolutely.
+   * @param newLine A boolean flag that determines whether /n characters are appended
+   *                at the end of lines, or not
+   * @return A single string containing all of the file contents.
+   * @throws IOException In case the file specified is not found.
+   */
   public static String readFileUTF8(String filePath, boolean newLine) throws IOException {
     Charset UTF8 = StandardCharsets.UTF_8;
     InputStream input = new FileInputStream(filePath);
@@ -63,7 +94,15 @@ public class FileTools {
     return fileText.toString();
   }
 
-  public static trecTopic[] openTrecEvalFile(String filePath) throws IOException {
+  /**
+   * A specialised method that opens a TREC formatted search results file, and fills the relevant
+   * information into (at most) 200 TREC topics.
+   * @param filePath The filepath specified absolutely.
+   * @return A trecTopic array consisting of all the TREC topics, their documents, scores, and
+   *         rankings inherent in the trec data structure.
+   * @throws IOException In case the file specified is not found.
+   */
+  public static trecTopic[] openTrecSearchResultsFile(String filePath) throws IOException {
     String trecResults = readFileUTF8(filePath, true);
     // TODO: Regex
     String[] lines = trecResults.split("\n");
@@ -87,65 +126,188 @@ public class FileTools {
     }
 
   /**
+   * Opens a TREC scores text and creates a trecResult object.
    * It's pretty inefficient.
-   * @param filePath is the fully qualified path to the trec results file.
+   * @param scoresText Text to de-serialize.
    * @return a trecResult object, containing all pertinent data for usage.
-   * @throws IOException In case the correct file is not found.
    */
-  public static trecResult openTrecResultsFile(String filePath) throws IOException {
-    String trecResults = readFileUTF8(filePath, true);
-    trecResults = trecResults.replace("\n", " ");
-    trecResults = trecResults.replaceAll("\t+", " ");
-    trecResults = trecResults.replaceAll(" +", " ");
-    String[] trecResultsFile = trecResults.split(" ");
-    trecResult tr = new trecResult(trecResultsFile[2],
-        Integer.parseInt(trecResultsFile[5]),
-        Integer.parseInt(trecResultsFile[8]),
-        Integer.parseInt(trecResultsFile[11]),
-        Integer.parseInt(trecResultsFile[14]),
-        Double.parseDouble(trecResultsFile[17]),
-        Double.parseDouble(trecResultsFile[20]),
-        Double.parseDouble(trecResultsFile[23]),
-        Double.parseDouble(trecResultsFile[26]),
-        Double.parseDouble(trecResultsFile[29]),
-        Double.parseDouble(trecResultsFile[32]),
-        Double.parseDouble(trecResultsFile[35]),
-        Double.parseDouble(trecResultsFile[38]),
-        Double.parseDouble(trecResultsFile[41]),
-        Double.parseDouble(trecResultsFile[44]),
-        Double.parseDouble(trecResultsFile[47]),
-        Double.parseDouble(trecResultsFile[50]),
-        Double.parseDouble(trecResultsFile[53]),
-        Double.parseDouble(trecResultsFile[56]),
-        Double.parseDouble(trecResultsFile[59]),
-        Double.parseDouble(trecResultsFile[62]),
-        Double.parseDouble(trecResultsFile[65]),
-        Double.parseDouble(trecResultsFile[68]),
-        Double.parseDouble(trecResultsFile[71]),
-        Double.parseDouble(trecResultsFile[74]),
-        Double.parseDouble(trecResultsFile[77]),
-        Double.parseDouble(trecResultsFile[80]),
-        Double.parseDouble(trecResultsFile[83]),
-        Double.parseDouble(trecResultsFile[86]),
-        Double.parseDouble(trecResultsFile[89]),
+  public static trecResult openTrecScoresFile(String scoresText) {
+    scoresText = scoresText.replace("\n", " ");
+    scoresText = scoresText.replaceAll("\t+", " ");
+    scoresText = scoresText.replaceAll(" +", " ");
+    String[] scores = scoresText.split(" ");
+    trecResult tr = new trecResult(scores[2],
+        Integer.parseInt(scores[5]),
+        Integer.parseInt(scores[8]),
+        Integer.parseInt(scores[11]),
+        Integer.parseInt(scores[14]),
+        Double.parseDouble(scores[17]),
+        Double.parseDouble(scores[20]),
+        Double.parseDouble(scores[23]),
+        Double.parseDouble(scores[26]),
+        Double.parseDouble(scores[29]),
+        Double.parseDouble(scores[32]),
+        Double.parseDouble(scores[35]),
+        Double.parseDouble(scores[38]),
+        Double.parseDouble(scores[41]),
+        Double.parseDouble(scores[44]),
+        Double.parseDouble(scores[47]),
+        Double.parseDouble(scores[50]),
+        Double.parseDouble(scores[53]),
+        Double.parseDouble(scores[56]),
+        Double.parseDouble(scores[59]),
+        Double.parseDouble(scores[62]),
+        Double.parseDouble(scores[65]),
+        Double.parseDouble(scores[68]),
+        Double.parseDouble(scores[71]),
+        Double.parseDouble(scores[74]),
+        Double.parseDouble(scores[77]),
+        Double.parseDouble(scores[80]),
+        Double.parseDouble(scores[83]),
+        Double.parseDouble(scores[86]),
+        Double.parseDouble(scores[89]),
         true
     );
     return tr;
   }
 
-  public static void writeTrecResult(trecTopic[] results) {
-    StringBuilder output = new StringBuilder();
+  /**
+   * Writes a TREC search result file to a specified location and appends relevant run information
+   * to the name of the file. As far as I know, this process is essential in order for comparing a
+   * TREC run with TREC eval executable.
+   * TODO: Specify a hashing algorithm to make matching values easier? But will that be necessary?
+   * @param results The results of the sorted trecTopics, as an array of trecTopic objects.
+   * @param runName The name of this run of tests.
+   * @param lambda1 The value of lambda1. (High coupling with my generic algorithms!)
+   * @param lambda2 The value of lambda2.
+   * @param centCutoff The value for the centrality cutoff.
+   */
+  public static void writeTrecSearchResultsFile(String filePath, trecTopic[] results,
+      String runName, String lambda1, String lambda2, String centCutoff) {
+    ArrayList<String> output = new ArrayList<>();
+
+    // Call toString method to get properly formatted output.
+    for (trecTopic result : results) {
+      output.add(result.toString());
+    }
+
+    // Send payload to this run's folder.
+    writeFile(output, fileNameSchema(GLOBALRESULTSPATH + runName, lambda1,
+        lambda2, centCutoff), null);
+  }
+
+  /**
+   * Writes a TREC scores result file to a specified location. Refer to writeTrecSearchResultsFile
+   * for more info.
+   * Having this information on file allows us to keep a set of scores from TREC eval as well as
+   * read from it for results. Inferred usage:
+   * -  write a TREC search result file.
+   * -  run TREC eval against this.
+   * -  read TREC eval against this using the openUTF8 method, obtain the string as desired
+   * and send it to this method. This method is actually just a file writer but keep it our little
+   * secret??
+   * @param text text to write as a TrecScoresFile.
+   * @param runName The name of this run of tests.
+   * @param lambda1 The value of lambda1.
+   * @param lambda2 The value of lambda2.
+   * @param centCutoff The value for the centrality cutoff.
+   * @param score This run's valuable score (can assume it's MAP).
+   */
+  public static void writeTrecScoresFile(String text, String runName, String lambda1,
+      String lambda2, String centCutoff, Double score) {
+    ArrayList<String> output = new ArrayList<>();
+    output.add(text);
+
+    // Send payload to newly created folder.
+    writeFile(output, fileNameSchema(GLOBALRESULTSPATH + runName, lambda1,
+        lambda2, centCutoff, score), null);
+  }
+
+  /**
+   * createFilePath simply creates a path for runs if the path does not exist at all.
+   * @param runName The name of this run of tests.
+   * @throws IllegalArgumentException in the case the path already exists.
+   */
+  private static void createFilePath (String runName) {
+    if (!(new File(GLOBALRESULTSPATH + runName).mkdirs())) throw new
+        IllegalArgumentException("path for: " + runName + " already exists and may contain"
+        + "files.");
+  }
+
+  /**
+   * Simply writes a file specified in the filePath.
+   * @param contents List of contents, which will be written to line by line.
+   * @param filePath Path of the file to write.
+   * @param charset Specified character set to write the file. Default UTF-8.
+   */
+  private static void writeFile(List<String> contents, String filePath, Charset charset) {
+    if (charset == null) charset = StandardCharsets.UTF_8;
+
+    try {
+      Files.write(Paths.get(filePath), contents, charset);
+    } catch (IOException ex) {
+      ex.printStackTrace();
+    }
 
   }
+
+  /**
+   * Refer to fileNameSchema, this method writes a TREC Search Results file.
+   * @param runName The name of this run of tests.
+   * @param lambda1 The value of lambda1.
+   * @param lambda2 The value of lambda2.
+   * @param centCutoff The value for the centrality cutoff.
+   */
+  private static String fileNameSchema(String runName, String lambda1, String lambda2,
+      String centCutoff) {
+
+    return "run- "
+        + runName
+        + ", l1- "
+        + lambda1
+        + ", l2- "
+        + lambda2
+        + ", cc- "
+        + centCutoff
+        + ".txt";
+  }
+
+  /**
+   * Helper method to create consistent file names. This method will write a consistent file
+   * name to match a TREC result file with its Eval sibling.
+   * Just a little note for me: If score is provided, then we know that this is printing out a
+   * Scores file.
+   * @param runName The name of this run of tests.
+   * @param lambda1 The value of lambda1.
+   * @param lambda2 The value of lambda2.
+   * @param centCutoff The value for the centrality cutoff.
+   * @param score Score is supplied when we are working with a TREC Scores file.
+   */
+  private static String fileNameSchema(String runName, String lambda1, String lambda2,
+      String centCutoff, double score) {
+
+    return "run- "
+        + runName
+        + ", score- "
+        + score
+        + ", l1- "
+        + lambda1
+        + ", l2- "
+        + lambda2
+        + ", cc- "
+        + centCutoff
+        + ".txt";
+  }
+
   public static void main(String[] args) throws IOException {
-    // simple test of the output of each of the file readers.
+    // Simple and manual test of the output of each of the file readers.
     String fileDir = "/home/luis-zugasti/IdeaProjects/tagme-luis/result_TF_IDF.txt";
     String ASCII = readFileASCII(fileDir);
     String UTF8 = readFileUTF8(fileDir, true);
-    trecTopic[] opentest = openTrecEvalFile(fileDir);
+    trecTopic[] opentest = openTrecSearchResultsFile(fileDir);
 
     String trecResultFileDir = "/home/luis-zugasti/IdeaProjects/tagme-luis/Luis_trec_results.txt";
-    trecResult tr = openTrecResultsFile(trecResultFileDir);
+    trecResult tr = openTrecScoresFile(trecResultFileDir);
     System.out.println(tr.toString());
     System.out.println("Testing completed.");
   }
